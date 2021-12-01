355d84172bcc807d34a242af30d1eeb4ab947f1f
horkoborko <47957160+horkoborko@users.noreply.github.com>
2021-12-01 13:29:19 -0700

    Creation of C and H file

---
+++ src/SQLiteUI.c
@@ -0,0 +1,19 @@
+/*
+ * SQLiteUI.c
+ *
+ *  Created on: Dec 1, 2021
+ *      Author: hannah
+ */
+
+#include "SQLiteUI.h"
+
+SQLiteUI::SQLiteUI()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+SQLiteUI::~SQLiteUI()
+{
+	// TODO Auto-generated destructor stub
+}
---
+++ src/SQLiteUI.h
@@ -0,0 +1,17 @@
+/*
+ * SQLiteUI.h
+ *
+ *  Created on: Dec 1, 2021
+ *      Author: hannah
+ */
+
+#ifndef SQLITEUI_H_
+#define SQLITEUI_H_
+
+class SQLiteUI {
+public:
+	SQLiteUI();
+	virtual ~SQLiteUI();
+};
+
+#endif /* SQLITEUI_H_ */
